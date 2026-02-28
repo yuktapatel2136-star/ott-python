e7997acef52af3270597149559b10d6bd88b8bc9
yukta <yuktapatel2136@gmail.com>
2026-02-28 19:45:42 +0530

    frist commit

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # 
+    from app.models.user_model import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    # 
+
+    from .routes.auth_route import main
+    app.register_blueprint(main)
+
+    return app
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
